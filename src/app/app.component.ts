8fbbcfa2886611f2e7115450f78ea3f8239220b6
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-01-27 15:52:37 -0300

    FEAT card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LinkBtnComponent } from '../link-btn/link-btn.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LinkBtnComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Social';
